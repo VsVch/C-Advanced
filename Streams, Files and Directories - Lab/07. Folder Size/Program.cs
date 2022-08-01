1f609df812df5d67021f77e8e2930eb89a70705c
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-08-01 19:45:51 +0300

    Add files via upload

--- Streams, Files and Directories - Lab/07. Folder Size/Program.cs
+++ Streams, Files and Directories - Lab/07. Folder Size/Program.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace _07._Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
